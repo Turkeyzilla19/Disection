10c79e0c8de358a874a90f96eb6ac713b52de1dd
Turkeyzilla19 <adrianaaron19@hotmail.com>
2017-07-03 19:48:44 -0400

    Update chippyRuxpin_audioPlayer.py

--- chippyRuxpin_audioPlayer.py
+++ chippyRuxpin_audioPlayer.py
@@ -25,27 +25,8 @@ class AudioPlayer:
         # Set up audio
         wavfile = wave.open(fileName,'r') #opens the wav file and sets it to read mode
         chunk = 1024 #creates a variable with a value of 1024
-        data = wavfile.readframes(chunk) #reads the wav file and creates a variable with those values. although it will  
-        try:
-          while data!='':
-             output.write(data)
-             # Split channel data and find maximum volume   
-             channel_l=audioop.tomono(data, 2, 1.0, 0.0)
-             channel_r=audioop.tomono(data, 2, 0.0, 1.0)
-             max_vol_factor =5000
-             max_l = audioop.max(channel_l,2)/max_vol_factor
-             max_r = audioop.max(channel_r,2)/max_vol_factor
-
-             for i in range (1,8):
-                self.generateMouthSignal((1<<max_r)-1)
-                
-             data = wavfile.readframes(chunk)
-        except:
-          data = None
+        data = wavfile.readframes(chunk) #reads the wav file and creates a variable with those values. although it will only go through 1024 or less frames  
         
-        os.system( '/etc/init.d/alsa-utils restart' )
-        sleep( .25 )
-
     def generateMouthSignal(self,val):
         delta = val - self.prevAudiovalue 
         if( delta < -2 or val == 0 ):
